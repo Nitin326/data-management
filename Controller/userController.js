2993f80e9b11690b0689809602d5cbd0262bc9a3
rahul meena <rahulbadgoti000@gmail.com>
2023-03-15 11:26:12 +0530

    list-index-search functionlity added

--- Controller/userController.js
+++ Controller/userController.js
@@ -13,10 +13,13 @@ let db = new sqlite3.Database('./database.db', sqlite3.OPEN_READWRITE, (err) => 
 
 
 const addUser = (req, res) => {
+    console.log("checking  createuser----------------------11");
     var name = req.body.username;
     var empid = req.body.empid;
     var userid = req.body.userid;
     var password = req.body.password;
+    console.log("checking  createuser----------------------22");
+
     db.run(`INSERT INTO mytable(name,empid,userid,password) VALUES(?,?,?,?)`, [name, empid, userid, password], function (err) {
         if (err) {
             return console.log(err.message);
@@ -25,6 +28,80 @@ const addUser = (req, res) => {
         console.log(`A row has been inserted `);
     });
     res.redirect('/login');
+}
+
+const table = (req, res) => {
+    var list = req.body.list;
+    var index = req.body.index;
+
+    console.log("list is running-----------------------",list);
+    console.log("index is running-----------------------",index);
+
+    //for creating coloum inside list
+    db.execute("ALTER TABLE List ADD COLUMN ?", (index))
+
+
+    //getting data from List table
+    db.all(`SELECT * FROM List`, (error, row) => {
+        if (error) {
+            console.log("erorrrr in table--userController----",error);
+        }
+        // console.log("list row is-------",row);
+        // res.render('tables',{values:row})
+
+        db.all(`SELECT * FROM addIndex`, (error, addIndexData) => {
+            if (error) {
+                console.log("erorrrr in table--userController----",error);
+            }
+            // console.log("addIndex row is-------",addIndexData);
+
+            let rowValues = {
+                listData : row,
+                indexData : addIndexData
+            }
+
+
+            res.render('tables',{recipes: rowValues})
+    
+        });
+    });
+
+        //getting data from addIndex table
+    // db.all(`SELECT * FROM addIndex`, (error, addIndexData) => {
+    //     if (error) {
+    //         console.log("erorrrr in table--userController----",error);
+    //     }
+    //     console.log("addIndex row is-------",addIndexData);
+    //     res.render('tables',{indexs:addIndexData})
+
+    // });
+
+}
+
+const Home = (req, res) => {
+    console.log("Home is==================-------");
+
+    //getting data from List table
+    db.all(`SELECT * FROM List`, (error, rows) => {
+        if (error) {
+            console.log("erorrrr in table--userController----",error);
+        }
+        console.log("list row is-------",rows);
+        res.render('Home', { home:rows })
+    });
+}
+
+const home_1 = (req, res) => {
+
+    // //getting data from List table
+    // db.all(`SELECT * FROM List`, (error, row) => {
+    //     if (error) {
+    //         console.log("erorrrr in table--userController----",error);
+    //     }
+    //     console.log("list row is-------",row);
+    //     // res.render('tables',{home:row})
+
+    // });
 }
 
 
@@ -65,17 +142,145 @@ const deleteUser = async (req,res,next) => {
     const id = req.params.id;
 }
 
-const getAlluser = async (req,res,next) => {
+const getAlluser = async (req,res) => {
     db.all("SELECT * FROM mytable", function(err, rows) {
         res.render('Alluser', {data:rows})
        });
 }
 
 
+// code for list 
+
+const listUser = (req, res) => {
+    var name = req.body.username;
+
+    let errors = [];
+
+    if (!name) {
+        errors.push({ msg: 'please fill the field' });
+    }
+
+    if (errors.length > 0) {
+        res.render('List', {
+            errors, name
+        })
+    }
+    else {
+
+    db.run("CREATE TABLE IF NOT EXISTS List (sn INTEGER PRIMARY KEY, name TEXT)");
+
+        
+    db.all(`SELECT * FROM List WHERE name = ?`,name, (error, row,) => {
+        console.log("row======================================",row );
+
+
+        let object = Object.assign({}, ...row)
+        console.log("object is --------------", object);
+        console.log("object is --------------", object.name == name);
+
+        if(object.name == name){
+        console.log("inside if-----");
+        //make alert section
+        res.redirect('/list')
+        
+        }
+    else{
+            console.log("insde else---------------------------------");     
+            db.run(
+              `INSERT INTO List (name) VALUES (?)`,
+              [name],
+              function (error) {
+              if (error) {
+                  console.error(error.message);
+              }
+              console.log(`Inserted a row with the ID: ${this.lastID}`);
+              }
+          );  
+
+
+            db.exec(`
+            CREATE TABLE ${name}
+            (
+              ID INTEGER PRIMARY KEY AUTOINCREMENT
+            );
+          `
+          );
+
+        }
+
+  });
+    }
+}
+
+
+
+const userindex = (req, res) => {
+    var name = req.body.username;
+
+    let errors = [];
+
+    if (!name) {
+        errors.push({ msg: 'please fill the field' });
+    }
+
+    if (errors.length > 0) {
+        res.render('addIndex', {
+            errors, name
+        })
+    }
+    else {
+
+    db.run("CREATE TABLE IF NOT EXISTS addIndex (sn INTEGER PRIMARY KEY, name TEXT)");
+
+        
+    db.all(`SELECT * FROM addIndex WHERE name = ?`,name, (error, row,) => {
+        console.log("row=============addIndex=========================",row );
+
+
+        let object = Object.assign({}, ...row)
+        console.log("object is --------------", object);
+        console.log("object is --------------", object.name == name);
+
+        if(object.name == name){
+        console.log("inside if----addIndex-");
+        //make alert section
+        res.redirect('/userindex')
+        
+        }
+    else{
+            console.log("insde else-------------addIndex--------------------");     
+            db.run(
+              `INSERT INTO addIndex (name) VALUES (?)`,
+              [name],
+              function (error) {
+              if (error) {
+                  console.error(error.message);
+              }
+              console.log(`Inserted a row with addIndex the ID: ${this.lastID}`);
+              }
+          );  
+
+        }
+
+  });
+    }
+}
+
+
+
+
+
+
+
+exports.Home = Home;
+exports.home_1 = home_1;
 exports.addUser = addUser;
 exports.loginUser = loginUser;
 exports.deleteUser = deleteUser;
 exports.getAlluser = getAlluser;
+exports.listUser = listUser;
+exports.userindex = userindex;
+exports.table = table;
 
 
 
--- Routes/Route.js
+++ Routes/Route.js
@@ -8,6 +8,22 @@ router.get('/', (req, res) => {
     res.render('Home');
 })
 
+router.post('/', userController.Home)
+
+router.get('/home_1', (req, res) => {
+    res.render('Home_1');
+})
+
+router.post('/home_1', userController.home_1)
+
+
+router.get('/table', (req, res) => {
+    res.render('tables');
+})
+
+router.post('/table', userController.table)
+
+
 router.get('/createuser', (req, res) => {
     res.render('Createuser');
 })
@@ -26,6 +42,22 @@ router.get('/login', (req, res) => {
 router.post('/login', userController.loginUser);
 
 router.get('/Alluser', userController.getAlluser)
+
+router.get('/list', (req, res) => {
+    res.render('List');
+})
+
+router.post('/list', userController.listUser);
+
+router.get('/userindex', (req, res) => {
+    res.render('addIndex');
+})
+
+router.post('/userindex', userController.userindex);
+
+// router.get('/searchuser', (req, res) => {
+//     res.render('searchUser');
+// })
 
 // router.get('/edituser', (req, res) => {
 //     res.render('editUser');
